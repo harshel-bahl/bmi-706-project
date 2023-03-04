33162a9268369fb49075d48bb4a081b05f12c3ab
Harshel Bahl <h.bahl.personal@gmail.com>
2023-03-04 17:15:42 -0500

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,11 @@ data = pd.read_csv("final_mass_data.csv")
 
 url  = "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/us-states/MA-25-massachusetts-counties.json"
 
-#data_map = alt.topo_feature("MA-25-massachusetts-counties.json", "cb_2015_massachusetts_county_20m")
+data_map = alt.topo_feature(url, "cb_2015_massachusetts_county_20m")
 
-base = alt.Chart("MA-25-massachusetts-counties.json").mark_geoshape(
+print(data_map)
+
+base = alt.Chart(data_map).mark_geoshape(
     fill='lightgray',
     stroke='white'
 ).project('mercator').properties(
