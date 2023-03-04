a8c1550c6d42bb03d3434092d9261d075dd37f2b
Harshel Bahl <h.bahl.personal@gmail.com>
2023-03-04 16:11:15 -0500

    map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,16 @@
 
+import altair as alt
+import pandas as pd
+import streamlit as st
+from vega_datasets import data
+
+
+states = alt.topo_feature(data.us_10m.url, feature='states')
+
+alt.Chart(states).mark_geoshape(
+    fill='lightgray',
+    stroke='white'
+).project('albersUsa').properties(
+    width=500,
+    height=300
+)
