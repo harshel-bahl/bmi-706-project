4256bb3058650e9b62f56848badccd8dd8382894
Harshel Bahl <h.bahl.personal@gmail.com>
2023-03-04 19:04:47 -0500

    c

--- pages/Intensity Distribution.py
+++ pages/Intensity Distribution.py
@@ -22,7 +22,7 @@ base = alt.Chart(data_map).mark_geoshape(
 points = alt.Chart(data.iloc[:500,]).mark_circle().encode(
     longitude='Longitude:Q',
     latitude='Latitude:Q',
-    color='Levels:Q',#alt.Color('Levels', type='quantitative'),
+    color=alt.Color('Levels', type='quantitative', scale=alt.Scale(scheme='accent')),
     tooltip='Towns'
 )
 
