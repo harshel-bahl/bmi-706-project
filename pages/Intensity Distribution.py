6829fb13b6200431b747d5883d8f7f96833fa54f
Harshel Bahl <h.bahl.personal@gmail.com>
2023-03-04 22:53:44 -0500

    c

--- pages/Intensity Distribution.py
+++ pages/Intensity Distribution.py
@@ -23,8 +23,8 @@ points = alt.Chart(data.iloc[:50000,]).mark_circle().encode(
     longitude='Longitude:Q',
     latitude='Latitude:Q',
     color=alt.Color('Levels', type='quantitative', scale=alt.Scale(scheme='blues', type='log', domain=(0.01,100), reverse=False), legend=alt.Legend(title="Contamination Levels")),
-    size=alt.value(50),
-    opacity=alt.value(0.3),
+    size=alt.value(30),
+    opacity=alt.value(0.1),
     tooltip='Towns'
 )
 
