2546db0424840e8de80f542214a9b61f21e03eee
Elombe Calvert <elombecalvert@hms.harvard.edu>
2023-03-05 00:17:20 -0500

    commit

--- Home.py
+++ Home.py
@@ -1,27 +1,30 @@
-import altair as alt
 import pandas as pd
+import altair as alt
+import json
+import requests
 import streamlit as st
 
-data = pd.read_csv("final_mass_data.csv")
 
+data = pd.read_csv("final_mass_data.csv")
 url  = "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/us-states/MA-25-massachusetts-counties.json"
 
 data_map = alt.topo_feature(url, "cb_2015_massachusetts_county_20m")
 
-# Intensity Map: intensity of PFAs overlayed across the base map of Massachussetts
-base = alt.Chart(data_map).mark_geoshape(
+Map = alt.Chart(data_map).mark_geoshape(
     fill='lightgray',
     stroke='white'
 ).project('mercator').properties(
     width=800,
-    height=600
-)
+    height=600)
 
-points = alt.Chart(data.iloc[:500,]).mark_circle().encode(
+points = alt.Chart(data.iloc[:3000,]).mark_circle(opacity=0.2).encode(
     longitude='Longitude:Q',
     latitude='Latitude:Q',
-    size=alt.value(20),
-    tooltip='Towns'
-)
+    #size=alt.Size("Levels:Q",legend=None),
+    color = alt.Color("Abbreviation:N",legend=None),
+  
+    size=alt.value(100),
+    tooltip=['Towns','Abbreviation:N'])
 
-st.altair_chart(base + points, use_container_width=True)
+Map_chart = Map + points
+st.altair_chart(Map_chart, use_container_width=True)
