abcd4bc3d40f8b6f03a179c7040cda877e0ddfd0
Harshel Bahl <h.bahl.personal@gmail.com>
2023-03-05 00:42:55 -0500

    c

--- pages/Intensity Distribution.py
+++ pages/Intensity Distribution.py
@@ -15,8 +15,8 @@ data_map = alt.topo_feature(url, "cb_2015_massachusetts_county_20m")
 # multi-selector to choose which years to display on chart
 selectedYears = st.multiselect("Years Shown", poss_years, poss_years[0], max_selections=3)
 subData = data[data["Year"].isin(selectedYears)]
-subData["Levels"] = (subData["Levels"]-subData["Levels"].min())/(subData["Levels"].max()-subData["Levels"].min())
-
+#subData["Levels"] = (subData["Levels"]-subData["Levels"].min())/(subData["Levels"].max()-subData["Levels"].min())
+#print(subData["Levels"].)
 # Intensity Map: intensity of PFAs overlayed across the base map of Massachussetts
 base = alt.Chart(data_map).mark_geoshape(
     fill='lightgray',
@@ -26,10 +26,13 @@ base = alt.Chart(data_map).mark_geoshape(
     height=600
 )
 
+levelsScale = alt.Scale(domain=[subData['Levels'].min(), subData['Levels'].max()], scheme='oranges')
+levelsColor = alt.Color(field='Levels', type='quantitative', scale=levelsScale)
+
 points = alt.Chart(subData).mark_circle().encode(
     longitude='Longitude:Q',
     latitude='Latitude:Q',
-    color=alt.Color('Levels'),#, scale=alt.Scale(reverse=True)),#, type='quantitative', scale=alt.Scale(scheme='blues', reverse=False), legend=alt.Legend(title="Contamination Levels")),
+    color=levelsColor,#scale=alt.Scale(reverse=True)),#, type='quantitative', scale=alt.Scale(scheme='blues', reverse=False), legend=alt.Legend(title="Contamination Levels")),
     size=alt.value(50),
     opacity=alt.value(1),
     tooltip='Towns'
