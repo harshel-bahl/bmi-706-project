573f0342c112c0f5d5f292f8b660a244be9db7c2
Harshel Bahl <h.bahl.personal@gmail.com>
2023-03-05 01:06:54 -0500

    c

--- pages/Intensity Distribution.py
+++ pages/Intensity Distribution.py
@@ -27,7 +27,7 @@ base = alt.Chart(data_map).mark_geoshape(
 )
 
 # add threshold levels in place of quantiles for interpretability
-levelsScale = alt.Scale(domain=[subData['Levels'].quantile(0.1), subData['Levels'].quantile(0.9)], scheme='oranges', clamp=True)
+levelsScale = alt.Scale(domain=[subData['Levels'].quantile(0.1), subData['Levels'].quantile(1)], scheme='oranges', clamp=True)
 levelsColor = alt.Color(field='Levels', type='quantitative', scale=levelsScale, legend=alt.Legend(title="Contamination Levels"))
 
 points = alt.Chart(subData).mark_circle().encode(
