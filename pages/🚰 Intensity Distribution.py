67dc2b525d907adceed2b96fdef13658819e1f23
Harshel Bahl <h.bahl.personal@gmail.com>
2023-03-07 22:45:19 -0500

    c

--- pages/🚰 Intensity Distribution.py
+++ pages/🚰 Intensity Distribution.py
@@ -199,7 +199,7 @@ elif len(selectedYears)==2:
         chart1 = chart + createChart(subData[subData["Year"]==selectedYears[0]], markSchemes[0])
         chart2 = chart + createChart(subData[subData["Year"]==selectedYears[1]], markSchemes[1])
         st.altair_chart(chart1)
-        #st.altair_chart(chart2, use_container_width=True)
+        st.altair_chart(chart2, use_container_width=True)
 
 elif len(selectedYears)==3:
     with st.container:
