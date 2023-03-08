62e1c2d10b04fab67d2d10cb85eacdea3a66f14d
Harshel Bahl <h.bahl.personal@gmail.com>
2023-03-07 23:41:24 -0500

    c

--- pages/🚰 Intensity Distribution.py
+++ pages/🚰 Intensity Distribution.py
@@ -190,18 +190,18 @@ def createChart(inputData, markScheme, colorScheme):
 
 if len(selectedYears)==1:
 
-    st.markdown('###'+str(selectedYears[0]))
+    st.markdown('### '+ str(selectedYears[0]))
     chart1 = base + createChart(subData[subData["Year"]==selectedYears[0]], markSchemes[0], "blues")
     st.altair_chart(chart1, use_container_width=True)
 
 
 if len(selectedYears)==2:
 
-    st.markdown('###'+str(selectedYears[0]))
+    st.markdown('### '+str(selectedYears[0]))
     chart1 = base + createChart(subData[subData["Year"]==selectedYears[0]], markSchemes[0], "blues")
     st.altair_chart((chart1), use_container_width=True)
 
-    st.markdown('###'+str(selectedYears[1]))
+    st.markdown('### '+str(selectedYears[1]))
     chart2 = base + createChart(subData[subData["Year"]==selectedYears[1]], markSchemes[1], "oranges")
     st.altair_chart((chart2), use_container_width=True)
     
