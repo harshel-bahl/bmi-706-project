89ab260d52f73a41174385089ec42bc3009014fa
Harshel Bahl <h.bahl.personal@gmail.com>
2023-03-07 22:38:03 -0500

    c

--- pages/🚰 Intensity Distribution.py
+++ pages/🚰 Intensity Distribution.py
@@ -195,13 +195,15 @@ if len(selectedYears)==1:
     st.altair_chart(chart1, use_container_width=True)
 
 elif len(selectedYears)==2:
-    chart1 = chart + createChart(subData[subData["Year"]==selectedYears[0]], markSchemes[0])
-    chart2 = chart + createChart(subData[subData["Year"]==selectedYears[1]], markSchemes[1])
-    st.altair_chart(chart1 | chart2, use_container_width=True)
+    with st.container:
+        chart1 = chart + createChart(subData[subData["Year"]==selectedYears[0]], markSchemes[0])
+        chart2 = chart + createChart(subData[subData["Year"]==selectedYears[1]], markSchemes[1])
+        st.altair_chart(chart1 | chart2, use_container_width=True)
 
 elif len(selectedYears)==3:
-    chart1 = chart + createChart(subData[subData["Year"]==selectedYears[0]], markSchemes[0])
-    chart2 = chart + createChart(subData[subData["Year"]==selectedYears[1]], markSchemes[1])
-    chart3 = chart + createChart(subData[subData["Year"]==selectedYears[2]], markSchemes[2])
-    st.altair_chart(chart1 | chart2 | chart3, use_container_width=True)
+    with st.container:
+        chart1 = chart + createChart(subData[subData["Year"]==selectedYears[0]], markSchemes[0])
+        chart2 = chart + createChart(subData[subData["Year"]==selectedYears[1]], markSchemes[1])
+        chart3 = chart + createChart(subData[subData["Year"]==selectedYears[2]], markSchemes[2])
+        st.altair_chart(chart1 | chart2 | chart3, use_container_width=True)
 
